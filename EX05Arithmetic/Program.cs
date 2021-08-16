915574cb6901322e7f623f7e0642e594182ad859
emil41m5 <emil41m5@CV-BB-5340>
2021-08-16 10:51:20 +0200

    Add project files.

---
+++ EX05Arithmetic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EX05Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lommeregner");
+            Console.WriteLine("Skriv det første tal.");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Skriv + for at lægge sammen.");
+            Console.WriteLine("Skriv - for at trække fra.");
+            Console.WriteLine("Skriv * for at gange.");
+            Console.WriteLine("Skriv / for at dividere.");
+            Console.WriteLine("Skriv % for modulo.");
+            string z = Console.ReadLine();
+            Console.WriteLine("Skriv det andet tal.");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (z == "+")
+            {
+                Console.WriteLine("Svar: {0}", a + b);
+            }
+            if (z == "-")
+            {
+                Console.WriteLine("Svar: {0}", a - b);
+            }
+            if (z == "*")
+            {
+                Console.WriteLine("Svar: {0}", a * b);
+            }
+            if (z == "/")
+            {
+                Console.WriteLine("Svar: {0}", a / b);
+            }
+            if (z == "%")
+            {
+                Console.WriteLine("Svar: {0}", a % b);
+            }
+        }
+    }
+}
